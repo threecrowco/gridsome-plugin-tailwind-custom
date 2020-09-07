e4e037895423d531fba37fccd3d3e4b796e85f26
threecrowco <studio@threecrow.co>
2020-09-07 01:31:45 -0500

    removed tailwindcss include from gridsome.client.js

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,59 +1,47 @@
 function TailwindPlugin(api, options) {
-  const {
-    tailwindConfig,
-    presetEnvConfig,
-    importUrlConfig,
-    shouldImport,
-    shouldTimeTravel,
-  } = options;
+    const { tailwindConfig, presetEnvConfig, importUrlConfig, shouldImport, shouldTimeTravel } = options
 
-  const tailwind = tailwindConfig
-    ? require("tailwindcss")(tailwindConfig)
-    : require("tailwindcss");
+    const tailwind = tailwindConfig ? require("tailwindcss")(tailwindConfig) : require("tailwindcss")
 
-  api.chainWebpack((config) => {
-    ["css", "scss", "sass", "less", "stylus", "postcss"].forEach((lang) => {
-      config.module
-        .rule(lang)
-        .oneOf("normal")
-        .use("postcss-loader")
-        .tap((options) => {
-          if (shouldTimeTravel) {
-            options.plugins.unshift(
-              require("postcss-preset-env")(presetEnvConfig)
-            );
-          }
+    api.chainWebpack((config) => {
+        ;["css", "scss", "sass", "less", "stylus", "postcss"].forEach((lang) => {
+            config.module
+                .rule(lang)
+                .oneOf("normal")
+                .use("postcss-loader")
+                .tap((options) => {
+                    if (shouldTimeTravel) {
+                        options.plugins.unshift(require("postcss-preset-env")(presetEnvConfig))
+                    }
 
-          options.plugins.unshift(tailwind);
+                    options.plugins.unshift(tailwind)
 
-          if (shouldImport) {
-            options.plugins.unshift(require("postcss-import")());
-            options.plugins.unshift(
-              require("postcss-import-url")(importUrlConfig)
-            );
-          }
+                    if (shouldImport) {
+                        options.plugins.unshift(require("postcss-import")())
+                        options.plugins.unshift(require("postcss-import-url")(importUrlConfig))
+                    }
 
-          return options;
-        });
-    });
-  });
+                    return options
+                })
+        })
+    })
 }
 
 TailwindPlugin.defaultOptions = () => ({
-  shouldImport: true,
-  shouldTimeTravel: true,
-  importUrlConfig: {
-    modernBrowser: true,
-  },
-  tailwindConfig: undefined,
-  presetEnvConfig: {
-    stage: 0,
-    autoprefixer: false,
-    features: {
-      "focus-visible-pseudo-class": false,
-      "focus-within-pseudo-class": false,
+    shouldImport: true,
+    shouldTimeTravel: true,
+    importUrlConfig: {
+        modernBrowser: true,
     },
-  },
-});
+    tailwindConfig: undefined,
+    presetEnvConfig: {
+        stage: 0,
+        autoprefixer: false,
+        features: {
+            "focus-visible-pseudo-class": false,
+            "focus-within-pseudo-class": false,
+        },
+    },
+})
 
-module.exports = TailwindPlugin;
+module.exports = TailwindPlugin
